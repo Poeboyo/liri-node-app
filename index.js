30645283a47336ce40d998530c46a0965c517b5f
Jeffrey Poe <jeffreypoe@jeffreys-mbp-2.lan>
2019-11-21 14:33:33 -0500

    Completed Switch Case, Working on ARGV input

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const keys = require("./keys.js");
 
 let command = process.argv[2];
 
-let input = process.argv[(3, process.argv.length)];
-console.log(input);
+let input = process.argv[3];
 
 switch (command) {
   case "concert-this":
@@ -20,7 +19,26 @@ switch (command) {
     axios
       .get("http://www.omdbapi.com/?i=tt3896198&apikey=a3207054&t=" + input)
       .then(function(response) {
-        console.log(`${response.title}`);
+        let movieTitle = response.data.Title;
+        let movieYear = response.data.Year;
+        let movieRatingImdb = response.data.Ratings[0].Value;
+        // let rottenTomaotoes = response.dataRatings[1].Value;
+        let movieProduceCountry = response.data.Production; //If return underined make witty message
+        let movieLang = response.data.Language;
+        let plotThiccens = response.data.Plot;
+        let actors = response.data.Actors;
+        console.log(
+          `\n---------------------------
+            
+            \nMovie Title: ${movieTitle}
+            \nYear: ${movieYear}
+            \nIMDB Rating: ${movieRatingImdb}
+            \nCountry: ${movieProduceCountry}
+            \nLanguage: ${movieLang}
+            \nPlot: ${plotThiccens}
+            \nActors/Actresses: ${actors}
+            \n---------------------------`
+        );
       });
     break;
 
